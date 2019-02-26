d4b8819cc9c858ae684cb808df05f4e7b18d3924
geesun56 <geesun56@gmail.com>
2019-02-26 11:49:02 -0500

    02/26/20190-React-app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from "./mm_header"
-import Content from "./mm_content"
-
+import Header from "./components/mm_header"
+import Overview from "./components/OVERVIEW"
+import Table_sample from "./components/tr_table"
+import {BrowserRouter, Route, Switch } from "react-router-dom";
+import Acc_info from "./components/ACC_INFO"
 const user_profile = [
   "Geesun Jang"
 ];
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+    <BrowserRouter>
+        <div>
         <Header/>
-        <Content/>
+        <Switch>
+            <Route path="/" component={Overview} exact/>
+            <Route path="/OVERVIEW" component={Overview}/>
+            <Route path="/ACC_INFO" component={Acc_info}/>
+        </Switch>
+        </div>
+      </BrowserRouter>
+     
       </div>
     );
   }
